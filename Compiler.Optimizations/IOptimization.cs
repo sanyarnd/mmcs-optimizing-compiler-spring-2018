20aa0bf8bde77c7f4366dbc42052ff3c8b420c52
Alexander Birukov <sanya.rnd@gmail.com>
2018-03-04 22:08:04 +0300

    Мелкие правки, документация, ошибки переопределения стандартных методов

--- Compiler.Optimizations/IOptimization.cs
+++ Compiler.Optimizations/IOptimization.cs
@@ -3,9 +3,17 @@ using Compiler.ThreeAddrCode.Nodes;
 
 namespace Compiler.Optimizations
 {
+    /// <summary>
+    ///     Интерфейс для оптимизаторов кода
+    /// </summary>
     public interface IOptimization
     {
+        /// <summary>
+        ///     Выполнить оптимизацию
+        /// </summary>
+        /// <param name="nodes">входные строки кода</param>
+        /// <param name="applied">была ли применена оптимизация</param>
+        /// <returns>результат применения оптимизации</returns>
         List<Node> Optimize(List<Node> nodes, out bool applied);
-
     }
 }

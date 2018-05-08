5766c0f49518a4e94b03f313d59a0491e3d81728
Naseka Andrew <andrew.naseka@yandex.ru>
2018-05-08 10:14:44 -0800

    Добавил файлы из master

--- Compiler.ThreeAddrCode.Tests/ReachingDefenitionTests.cs
+++ Compiler.ThreeAddrCode.Tests/ReachingDefenitionTests.cs
@@ -1,14 +1,19 @@
-﻿using Compiler.ThreeAddrCode.Expressions;
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Compiler.ThreeAddrCode.Expressions;
 using Compiler.ThreeAddrCode.Nodes;
-using NUnit.Framework;
+using Compiler.ThreeAddrCode.CFG;
+using Compiler.ThreeAddrCode.DFA.ReachingDefinitions;
 
 namespace Compiler.ThreeAddrCode.Tests
 {
     [TestFixture]
-    public class TaCodeTests
+    class ReachingDefenitionTests
     {
         [Test]
-        public void Test1()
+        public void ReachingDefenitionTest1()
         {
             var taCode = new TACode();
 
@@ -19,7 +24,6 @@ namespace Compiler.ThreeAddrCode.Tests
                 Right = new IntConst(5),
                 Result = new Var()
             };
-
             var ass2 = new Assign
             {
                 Left = new IntConst(10),
@@ -27,21 +31,18 @@ namespace Compiler.ThreeAddrCode.Tests
                 Right = new IntConst(2),
                 Result = new Var()
             };
-
             var ass3 = new Assign
             {
                 Operation = OpCode.Minus,
                 Right = new IntConst(1),
                 Result = new Var()
             };
-
             var ifgt1 = new IfGoto
             {
                 Condition = new IntConst(1),
                 TargetLabel = ass3.Label
             };
             ass3.IsLabeled = true;
-
             var ass4 = new Assign
             {
                 Left = ass3.Result,
@@ -49,14 +50,12 @@ namespace Compiler.ThreeAddrCode.Tests
                 Right = new IntConst(1999),
                 Result = new Var()
             };
-
             var ifgt2 = new IfGoto
             {
                 Condition = new IntConst(2),
                 TargetLabel = ass2.Label
             };
             ass2.IsLabeled = true;
-
             var ass5 = new Assign
             {
                 Left = new IntConst(7),
@@ -64,7 +63,6 @@ namespace Compiler.ThreeAddrCode.Tests
                 Right = new IntConst(4),
                 Result = new Var()
             };
-
             var ass6 = new Assign
             {
                 Left = new IntConst(100),
@@ -81,8 +79,20 @@ namespace Compiler.ThreeAddrCode.Tests
             taCode.AddNode(ifgt2);
             taCode.AddNode(ass5);
             taCode.AddNode(ass6);
-            
-            // TODO Assert'ы
+
+            var cfg = new ControlFlowGraph(taCode);
+            taCode.CreateBasicBlockList();
+
+            var op = new Operations(taCode);
+            var tf = new TransferFunction();
+            var outset = tf.Transfer(cfg.CFGNodes.ElementAt(0), op.Lower, op);
+
+            Assert.AreEqual(cfg.CFGNodes.ElementAt(0).CodeList.Count(), outset.Count);
+            Assert.IsEmpty(outset.Except(cfg.CFGNodes.ElementAt(0).CodeList.Select(x => (Guid)x.Label)));
+
+            var iter = new IterativeAlgorithm();
+            var inout = iter.Analyze(cfg, op, tf);
+            // TODO: тестирование
         }
-	}
+    }
 }
--- Compiler.ThreeAddrCode/Nodes/Node.cs
+++ Compiler.ThreeAddrCode/Nodes/Node.cs
@@ -8,15 +8,10 @@ namespace Compiler.ThreeAddrCode.Nodes
     /// </summary>
     public class Node
     {
-        public Node()
-        {
-            Label = Guid.NewGuid();
-        }
-
         /// <summary>
         ///     Уникальная метка-идентификатор
         /// </summary>
-        public Guid Label { get; set; }
+        public PrettyGuid Label { get; set; }
 
         /// <summary>
         ///     К какому базовому блоку принадлежит строка
@@ -27,17 +22,33 @@ namespace Compiler.ThreeAddrCode.Nodes
         ///     Флаг наличия перехода по goto на эту строку кода
         /// </summary>
         public bool IsLabeled { get; set; }
+        
+        public Node(string name = "")
+        {
+            Label = new PrettyGuid(IDType.Label, name);
+        }
 
         public override bool Equals(object obj)
         {
-            if (obj is Node node)
-                return Label == node.Label;
-            return false;
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Label.Equals(((Node) obj).Label);
         }
 
         public override int GetHashCode()
         {
             return Label.GetHashCode();
+        }
+
+        public static bool operator ==(Node left, Node right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Node left, Node right)
+        {
+            return !Equals(left, right);
         }
     }
 }
